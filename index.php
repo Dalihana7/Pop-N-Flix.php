7dcf4ddc5417e5f3f80ed13ffdf79acdd7a4b204
Myriam-cyber22 <myriam.akerdine@supinfo.com>
2026-01-30 23:18:34 +0100

    index.php

--- index.php
+++ index.php
@@ -19,11 +19,15 @@ include 'includes/header.php';
             <?php foreach ($films as $film): ?>
                 <div class="movie-card">
                     <a href="movie.php?id=<?php echo $film['id']; ?>" class="movie-card-link">
-                        <img src="<?php echo htmlspecialchars($film['image_url']); ?>" alt="<?php echo htmlspecialchars($film['titre']); ?>">
+                        <?php if (!empty($film['image_url'])): ?>
+                            <img src="<?php echo htmlspecialchars($film['image_url']); ?>" alt="<?php echo htmlspecialchars($film['titre'] ?? ''); ?>">
+                        <?php else: ?>
+                            <img src="https://via.placeholder.com/300x450?text=No+Image" alt="<?php echo htmlspecialchars($film['titre'] ?? ''); ?>">
+                        <?php endif; ?>
                         
                         <div class="movie-info-bottom">
-                            <h3><?php echo htmlspecialchars($film['titre']); ?></h3>
-                            <p>Prix : <?php echo number_format($film['prix'], 2); ?>€</p>
+                            <h3><?php echo htmlspecialchars($film['titre'] ?? 'Sans titre'); ?></h3>
+                            <p>Prix : <?php echo isset($film['prix']) ? number_format($film['prix'], 2) : '0.00'; ?>€</p>
                         </div>
                     </a>
                 </div>
