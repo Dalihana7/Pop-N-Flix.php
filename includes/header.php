cefa3f6380b20ec6c0e63078a36b89199dc89ff5
Myriam-cyber22 <myriam.akerdine@supinfo.com>
2026-01-30 23:14:45 +0100

    header.php

--- includes/header.php
+++ includes/header.php
@@ -4,23 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pop'N'Flix - Films & Séries</title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
     <header class="main-header">
         <div class="logo-container">
-            <img src="https://cdn-icons-png.flaticon.com/512/3418/3418886.png" class="popcorn-icon">
+            <img src="https://cdn-icons-png.flaticon.com/512/3418/3418886.png" class="popcorn-icon" alt="Popcorn">
             <h1 class="main-title">Pop'N'Flix</h1>
             <div class="red-divider"></div>
         </div>
         <nav class="centered-nav">
             <a href="index.php">Accueil</a>
-            <a href="category.php?type=action">Action</a>
-            <a href="category.php?type=drama">Drame</a>
-            <a href="category.php?type=action">Horreur</a>
-            <a href="category.php?type=action">Animation</a>
+            <a href="category.php?type=Action">Action</a>
+            <a href="category.php?type=Drame">Drame</a>
+            <a href="category.php?type=Horreur">Horreur</a>
+            <a href="category.php?type=Animation">Animation</a>
             <a href="search.php">Recherche</a>
-            <a href="login.php" id="btn-connexion">Se connecter</a>
-            <a href="cart.php">Panier</a>
+            <?php if(isset($_SESSION['utilisateur_id'])): ?>
+                <a href="panier.php">Panier</a>
+                <a href="historique.php">Historique</a>
+                <a href="deconnexion.php">Déconnexion</a>
+            <?php else: ?>
+                <a href="connexion.php">Se connecter</a>
+                <a href="inscription.php">S'inscrire</a>
+            <?php endif; ?>
         </nav>
     </header>
